17e05b383758b0b8f3c1e9facbad6d93b38bbbd9
Shubham Mule <shubhambmule@gmail.com>
2021-11-19 00:39:41 +0530

    Added : Welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQ_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LINQ Problems");
+        }
+    }
+}
